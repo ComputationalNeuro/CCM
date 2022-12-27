211b50af1ff4f40b59384d7d49b53fa7a942fed7
Luke Chen <lchen64@berkeley.edu>
2022-12-27 12:09:21 +0900

    Update Process.py

--- Process.py
+++ Process.py
@@ -1,8 +1,10 @@
 import numpy as np
-from scipy.io import loadmat  # SciPy module that loads mat-files
 import matplotlib.pyplot as plt
 from datetime import datetime, date, time
 import pandas as pd
+
+from scipy.io import loadmat  # SciPy module that loads mat-files
+from scipy.ndimage import gaussian_filter1d
 
 from pyEDM import *
 from causal_ccm import *
@@ -15,8 +17,25 @@ def get_single_trial_data(spikes, index):
         trial_spikes.append(train[index])
     return trial_spikes
 
+# Function for applying Gaussian Kernel as a Filter with Standard Deviation Sigma 
+def gaussian_filter(spikes, sigma):
+    smoothed = np.empty([585, 386523])
+    i = 0
+    for train in spikes:
+        smoothed[i] = gaussian_filter1d(train, sigma)
+        i+=1
+    return smoothed
+ 
+def filter_waldo_voltaire(neurons):
+    waldo = []
+    voltaire = []
+    for n in range(0,584):
+        if neurons[n][0] == 'W':
+            waldo.append(n)
+        if neurons[n][0] == 'v':
+            voltaire.append(n)
 
-# only include time bins 101 through 181
+# Only include time bins 101 through 181
 def filter_time_bins(spikes):
     relevant_spikes = []
     for train in spikes:
@@ -30,14 +49,14 @@ def filter_time_bins(spikes):
 
 # Function for Applying Moving Average Time Series Smoother
 def moving_average(spikes, kernel_size):
-    smoothed = np.empty([585, 2400])
+    smoothed = np.empty([585, spikes.shape[1]])
     i = 0
     for train in spikes:
         kernel = np.ones(kernel_size) / kernel_size
         smoothed[i] = np.convolve(train, kernel, mode='same')
         i+=1
     return smoothed
-    
+
 #get only spike trains from the last 30 trials after 20 trials of uncertainty
 def truncate(spikes):
     truncated = np.empty([585,2400])
@@ -45,8 +64,7 @@ def truncate(spikes):
     for train in spikes: 
         truncated[i] = train[4000:6400]
         i+=1
-
-#get sub-lists of neurons in letter groups
+    
 def get_neuron_groups(neurons):
 
     aNeurons = []
@@ -76,4 +94,4 @@ def get_neuron_groups(neurons):
         if neurons[n][1][0] == 'H':
             hNeurons.append(n)
 
-    return np.vstack((aNeurons, bNeurons, cNeurons, dNeurons, eNeurons, fNeurons, gNeurons, hNeurons))
+    return aNeurons, bNeurons, cNeurons, dNeurons, eNeurons, fNeurons, gNeurons, hNeurons
